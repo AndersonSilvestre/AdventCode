d1906057ecb30b6572281c6d80780394af4861d6
Anderson Silvestre <andersonsilvestre@hotmail.com.br>
2023-06-05 10:52:42 -0300

    Add files via upload

--- 2022/day2/day2.py
+++ 2022/day2/day2.py
@@ -1,19 +1,17 @@
 """
-Referencias
+Refs
 https://panda.ime.usp.br/pensepy/static/pensepy/10-Arquivos/files.html
-
 """
 import os
 
-dirname = os.path.dirname("/home/user/Área de Trabalho/adventCode2022/input/")
-filename = os.path.join(dirname, 'day2.txt')
+dirname = os.path.dirname("/home/anderson/Documentos/Programação/2022/day2/")
+file = os.path.join(dirname, 'day2.txt')
 
 # A and X = Rock(1)
 # B and Y = Paper(2)
 # C and Z = Scissors(3)
 # Score = Lose(0), Draw(3), Victory(6)
-elfo = []
-meu = []
+elfo, meu = [], []
 lose = 0
 draw = 3
 victory = 6
@@ -22,117 +20,58 @@ Y = 2
 Z = 3
 
 
-def readJokenpo(arquivo):
+def read_jokenpo(arquivo):
 	
-	arquivo = open(filename, 'r')
+	arquivo = open(arquivo, 'r')
 	item = 0	
 	for linha in arquivo.readlines():
 		elfo.append(linha[0])
 		meu.append(linha[2])
 	arquivo.close()
-'''
-	while item < len(elfo):
-		if elfo[item] == "A":
-			elfo[item] = 1
-			#print("igual A")
-			#print(item, "A")
-		if elfo[item] == "B":
-			elfo[item] = 2
-			#print(item, "B")
-		if elfo[item] == "C":
-			elfo[item] = 3
-			#print(item, "C")
-		item += 1
-	item = 0
 
-	while item < len(meu):
-		if meu[item] == "X":
-			meu[item] = 1
-			#print(item, "X")
-		if meu[item] == "Y":
-			meu[item] = 2
-			#print(item, "Y")
-		if meu[item] == "Z":
-			meu[item] = 3
-			#print(item, "Z")
-		item += 1
-'''
 
-def jokenpoLetras(player1, player2):
+def jokenpo_letras(player1, player2):
+	# Primeira parte / Part 1
 	score = []
 
 	for i in range(len(elfo)):
-		#Se o elfo jogar Pedra(A)
+		# Se o elfo jogar Pedra(A) / If elf choose Rock(A)
 		if player1[i] == "A" and player2[i] == "X":
 			score.append(draw + X)
-			#i += 1
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(draw, "+", player2[i], score)
-			
+
 		elif player1[i] == "A" and player2[i] == "Y":
 			score.append(victory + Y)
-			#i += 1
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(victory, "+", player2[i], score)
-			
+
 		elif player1[i] == "A" and player2[i] == "Z":
 			score.append(lose + Z)
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(lose, "+", player2[i], score)
-			
 
-		#Se o elfo jogar Papel(B)
+		# Se o elfo jogar Papel(B) / If elf choose Paper(B)
 		elif player1[i] == "B" and player2[i] == "X":
 			score.append(lose + X)
-			#i += 1
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(lose, "+", player2[i], score)
-			
+
 		elif player1[i] == "B" and player2[i] == "Y":
 			score.append(draw + Y)
-			#i += 1
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(draw, "+", player2[i], score)
-			
+
 		elif player1[i] == "B" and player2[i] == "Z":
 			score.append(victory + Z)
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(victory, "+", player2[i], score)
-			
 
-		#Se o elfo jogar Tesoura(C)
+		# Se o elfo jogar Tesoura(C) / If elf choose Scissors(C)
 		elif player1[i] == "C" and player2[i] == "X":
 			score.append(victory + X)
-			#i += 1
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(victory, "+", player2[i], score)
-			
+
 		elif player1[i] == "C" and player2[i] == "Y":
 			score.append(lose + Y)
-			#i += 1
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(lose, "+", player2[i], score)
-			
+
 		elif player1[i] == "C" and player2[i] == "Z":
 			score.append(draw + Z)
-			#print(i)
-			#print(player1[i], player2[i])
-			#print(draw, "+", player2[i], score)
-
 	print(sum(score))
 
-def jokenpoFinal(player1, player2):
+
+def jokenpo_final(player1, player2):
+	# Segunda Parte / Part two
 	score = []
 	for i in range(len(elfo)):
-		#Se o elfo jogar Pedra(A)
+		# Se o elfo jogar Pedra(A) / If elf choose Rock(A)
 		if player1[i] == "A":
 			if player2[i] == "X":
 				score.append(Z + lose)
@@ -140,7 +79,7 @@ def jokenpoFinal(player1, player2):
 				score.append(X + draw)
 			elif player2[i] == "Z":
 				score.append(Y + victory)
-		#Se o elfo jogar Papel(B)
+		# Se o elfo jogar Papel(B) / If elf choose Paper(B)
 		elif player1[i] == "B":
 			if player2[i] == "X":
 				score.append(X + lose)
@@ -148,7 +87,7 @@ def jokenpoFinal(player1, player2):
 				score.append(Y + draw)
 			elif player2[i] == "Z":
 				score.append(Z + victory)
-		#Se o elfo jogar Tesoura(C)
+		# Se o elfo jogar Tesoura(C) / If elf choose Scissors(C)
 		elif player1[i] == "C":
 			if player2[i] == "X":
 				score.append(Y + lose)
@@ -157,6 +96,8 @@ def jokenpoFinal(player1, player2):
 			elif player2[i] == "Z":
 				score.append(X + victory)
 	print(sum(score))
-readJokenpo(filename)
-jokenpoLetras(elfo, meu)
-jokenpoFinal(elfo, meu)
+
+
+read_jokenpo(file)
+jokenpo_letras(elfo, meu)
+jokenpo_final(elfo, meu)
