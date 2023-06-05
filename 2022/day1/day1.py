94fa2a61ef9031ea838ed02471401efeca012ffd
Anderson Silvestre <andersonsilvestre@hotmail.com.br>
2023-06-05 10:30:57 -0300

    Add files via upload

---
+++ 2022/day1/day1.py
@@ -0,0 +1,25 @@
+import os
+dirname = os.path.dirname("/home/anderson/Documentos/Programação/adventCode2022/input/")
+filename = os.path.join(dirname, 'day1.txt')
+
+
+def read_text(file):
+    maior = []
+    soma = 0
+
+    data = open(file, 'r')
+
+    for line in data.readlines():
+        if line[0] != '\n':
+            soma += int(line)
+        else:
+            maior.append(soma)
+            soma = 0
+            continue
+    print(max(maior))
+    # second answer
+    maior.sort(reverse=True)
+    print(sum(maior[0:3]))
+
+
+read_text(filename)
